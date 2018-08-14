1cd54e4a1313922fd240c89408b83a2b882e5e07
OGGonzalezM <ogm@soluciones4g.com>
2018-08-14 13:31:12 -0500

    [ADD] Added all the files

---
+++ quotations_image/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quotations with product image",
+
+    'summary': """
+        Add the product image to a quotation
+        """,
+
+    'description': """
+        To show the product image to a quotation
+    """,
+
+    'author': "Soluciones4G",
+    'website': "http://www.soluciones4g.com",
+
+    'category': 'Extra Tools',
+    'version': '0.1',
+
+    'depends': ['base', 'sale'],
+
+    'data': [
+        'report/quotations_extended_report.xml',
+    ],
+    
+    'installable':True,
+    'auto_install':False,
+}
